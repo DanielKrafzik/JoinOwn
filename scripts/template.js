9ded62d327952680a4bbdebc724add791575769b
danielkrafzik <daniel.krafzik@hotmail.de>
2025-06-30 22:30:05 +0200

    rendering of priority

--- scripts/template.js
+++ scripts/template.js
@@ -8,16 +8,29 @@ function userDropDownTemplate(name, inititals, index, id) {
             </div>`;
 }
 
-function ticketTemplate(title, description, category, categoryCss) {
+function ticketTemplate(title, description, category, categoryCss, assignedTo, priority) {
+    let userSpans = assignedTo.map((user, i) => {
+        let initials = user.split(" ").map(n => n[0]).join("").toUpperCase();
+        return `<span class="user-icon User-bc-${(i + 1) % 15}">${initials}</span>`;
+    }).join("");
+    
+
     return `
         <div class="kanban-task" onclick="popUpAddTask(popuptask)">
             <div class="task-type ${categoryCss}">${category}</div>
             <h4>${title}</h4>
             <p>${description}</p>
+            <div>
+                <div>
+                    <div></div>
+                </div>
+                <p></p>
+            </div>
             <div class="assigned-users">
               <div>
+              ${userSpans}
               </div>
-              <img src="" alt="" />
+              <img src="./assets/icon/${priority}.svg" alt="" />
             </div>
           </div>
     `
