efe819cfe684e4733bc3f92fd7e71741618550a5
danielkrafzik <daniel.krafzik@hotmail.de>
2025-09-01 23:54:51 +0200

    made madatory fields at addtasks mandatory

--- scripts/board.js
+++ scripts/board.js
@@ -23,8 +23,8 @@ let allTickets = JSON.parse(getTickets);
 /**
  * function to open/close the addTask pop-up
  */
-function popUpAddTask(ele) {
-  const isHidden = ele.classList.contains("hide");
+function popUpAddTask(ele, columnVal) {
+    const isHidden = ele.classList.contains("hide");
   if (
     document.getElementById("board-task-information").className === "hide" &&
     document.getElementById("board-task-edit").className === ""
@@ -44,7 +44,8 @@ function popUpAddTask(ele) {
       ele.classList.add("hide");
       overlay.classList.add("hide");
     }, 200);
-  }
+  }  
+  document.getElementById("create-task-button").dataset.column = columnVal || "To do";
 }
 
 /**
@@ -267,12 +268,8 @@ async function saveChangedTicketInFirbase() {
  */
 function toggleNoTaskContainer() {
   let allTicketsToDo = allTickets.filter((obj) => obj.column == "To do");
-  let allTicketsProgress = allTickets.filter(
-    (obj) => obj.column == "In progress"
-  );
-  let allTicketsFeedback = allTickets.filter(
-    (obj) => obj.column == "Await feedback"
-  );
+  let allTicketsProgress = allTickets.filter((obj) => obj.column == "In progress");
+  let allTicketsFeedback = allTickets.filter((obj) => obj.column == "Await feedback");
   let allTicketsDone = allTickets.filter((obj) => obj.column == "done");
   checkTicketsForToggle(allTicketsToDo, "noTasksToDo");
   checkTicketsForToggle(allTicketsProgress, "noTasksProgress");
